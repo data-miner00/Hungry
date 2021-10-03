82c130fb7ceca607edd7f1463c569e7df1dcc17f
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-03 19:53:39 +0800

    ✨ Updated routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\DelicaciesController;
+use App\Http\Controllers\OrdersController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +22,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hello', function (Request $request) {
-    return 'hello world';
+Route::post('/register', [UsersController::class, 'register']);
+Route::post('/login', [UsersController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::resource('delicacies', DelicaciesController::class);
+    Route::get('/delicacies/search/{name}', [DelicaciesController::class, 'search']);
+    Route::resource('orders', OrdersController::class);
+    Route::get('/orders/search/{user_id}', [OrdersController::class, 'search']);
+    Route::post('/logout', [UsersController::class, 'logout']);
 });
+
+// Route::get('/delicacies', [DelicaciesController::class, 'index']);
+// Route::post('/delicacies', [DelicaciesController::class, 'store']);
+
+// Route::get('/orders', [OrdersController::class, 'index']);
+// Route::get('/orders', [OrdersController::class, 'store']);
