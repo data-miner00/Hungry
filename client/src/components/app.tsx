ee024a753dc6963d13c17533183483d47edc8c2d
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-09 22:29:53 +0800

    ✨ Add new routes

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,23 +1,30 @@
 import { FunctionalComponent, h } from 'preact';
 import { Route, Router } from 'preact-router';
 
-import Home from '../routes/home';
 import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
+import Landing from '../routes/landing';
 import Header from './header';
+import Register from '../routes/register';
+import Dashboard from '../routes/user/dashboard';
 
 const App: FunctionalComponent = () => {
-    return (
-        <div id="preact_root">
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
+  return (
+    <div
+      id="preact_root"
+      className="container border border-gray-100 border-solid min-h-screen mx-auto"
+    >
+      <Header />
+      <Router>
+        <Route path="/" component={Landing} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/profile/" component={Profile} user="me" />
+        <Route path="/profile/:user" component={Profile} />
+        <NotFoundPage default />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
