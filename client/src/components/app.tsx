8e3a705e854656810a5211994bbc4814a7cc854b
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-16 19:33:41 +0800

    ✨ Mock Auth in routes

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -1,30 +1,36 @@
-import { FunctionalComponent, h } from 'preact';
+import { FunctionalComponent, h, Fragment } from 'preact';
 import { Route, Router } from 'preact-router';
 
 import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
 import Landing from '../routes/landing';
-import Header from './header';
 import Register from '../routes/register';
 import Dashboard from '../routes/user/dashboard';
 import DelicacyDetail from '../routes/user/delicacyDetails';
+import Orders from '../routes/user/orders';
+import { useState } from 'preact/hooks';
 
 const App: FunctionalComponent = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
-    <div
-      id="preact_root"
-      className="container border border-gray-100 border-solid min-h-screen mx-auto"
-    >
-      <Header />
-      <Router>
-        <Route path="/" component={Landing} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/profile/" component={Profile} user="me" />
-        <Route path="/profile/:user" component={Profile} />
-        <Route path="/delicacy/:id" component={DelicacyDetail} />
-        <NotFoundPage default />
-      </Router>
+    <div id="preact_root">
+      {!authenticated ? (
+        <Router>
+          <Route path="/" component={Landing} />
+          <Route path="/register" component={Register} />
+          <NotFoundPage default />
+        </Router>
+      ) : (
+        <Router>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/profile/" component={Profile} user="me" />
+          <Route path="/profile/:user" component={Profile} />
+          <Route path="/delicacy/:id" component={DelicacyDetail} />
+          <Route path="/orders" component={Orders} />
+          <NotFoundPage default />
+        </Router>
+      )}
     </div>
   );
 };
