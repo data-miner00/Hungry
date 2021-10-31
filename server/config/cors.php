82716160aacaadf52c17899030db7f1fe0a60483
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-31 17:56:58 +0800

    🔧 Scope cors headers

--- server/config/cors.php
+++ server/config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'],
 
     'exposed_headers' => [],
 
